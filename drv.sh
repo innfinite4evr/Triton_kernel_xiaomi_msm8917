0e38ff6e4cd1c714beebf5fb3ab3f41427f7209f
Thagoo <lohitgowda56@gmail.com>
2020-01-18 19:54:47 +0530

    Import video driver changes

--- drv.sh
+++ drv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git reset HEAD^ drivers/usb
+git reset HEAD^ drivers/video
 git commit --amend --no-edit
 git add --all -f
-git commit -a -m "Import usb driver changes"
+git commit -a -m "Import video driver changes"
 git rebase -i HEAD~2
