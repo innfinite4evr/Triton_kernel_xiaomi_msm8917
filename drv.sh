8b26030196ce47c11afe12575b55731cae0e2f86
Thagoo <lohitgowda56@gmail.com>
2020-01-18 19:52:00 +0530

    Import regulator driver changes

--- drv.sh
+++ drv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git reset HEAD^ drivers/misc
+git reset HEAD^ drivers/regulator
 git commit --amend --no-edit
 git add --all -f
-git commit -a -m "Import dts changes"
+git commit -a -m "Import regulator driver changes"
 git rebase -i HEAD~2
